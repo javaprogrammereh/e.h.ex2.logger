0bfbe23099b01c04fa0bc9b717d757cd40f3fd61
javaprogrammereh <38442252+javaprogrammereh@users.noreply.github.com>
2022-08-22 15:08:42 +0430

    Update singleLogCodeController.js

--- modules/controllers/api/v1/logDescription/singleLogCodeController.js
+++ modules/controllers/api/v1/logDescription/singleLogCodeController.js
@@ -5,19 +5,11 @@ module.exports = new ( class singleLogCodeController extends  InitializeControll
 
     let query = {};
     let sort = { _id: -1 };
-    //* /api/v1/logDescriptions/logcode?s=...
-    //*
+    //* /api/v1/logcode?s=...
     if (req.query.s !== undefined) {
       query = { ...query, $text: { $search: `/${req.query.s}/i` } };
       sort = { ...sort, score: { $meta: "textScore" } };
     }
-    // if (req.body.logcode  !== undefined) {
-    //   query = { ...query, $text: { $search: `/${req.body.logcode}/i` } };
-    //   sort = { ...sort, score: { $meta: "textScore" } };
-      // req.checkQuery("logcode", "The entered logcode is incorrect").isMongoId()
-      // if (this.validation(req, res)) return '';
-      // query={...query,logcode:req.body.logcode}
-    // }
       const aggregateData = [
         { $match: query },
         {
